0e501c4689fcded9176fd065a9cf3df73e02dce8
rapha <raphael.pavlidis@gmail.com>
2014-04-21 20:49:30 +0200

    TEST

--- src/File/Mastermind_File.java
+++ src/File/Mastermind_File.java
@@ -1,53 +1,29 @@
 package File;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.StreamCorruptedException;
 
 import Mastermind.Mastermind;
 
 public class Mastermind_File
 {
-	public static void Save_Mastermind(Mastermind mastermind, String path)
+	public static void Save_Mastermind(Mastermind mastermind, File file) throws IOException
 	{
-		try
-		{
-			FileOutputStream saveFile = new FileOutputStream(path);
-			ObjectOutputStream save = new ObjectOutputStream(saveFile);
-			save.writeObject(mastermind);	
-			save.close();
-		}
-		catch(Exception exc)
-		{
-			exc.printStackTrace();
-		}
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+		objectOutputStream.writeObject(mastermind);	
+		objectOutputStream.close();
 	}
 
-	public static Mastermind Load_Mastermind(String path)
+	public static Mastermind Load_Mastermind(File file) throws IOException, ClassNotFoundException
 	{	
-		ObjectInputStream save = null;
-		Mastermind mastermind = null;
-		try
-		{
-			FileInputStream saveFile = new FileInputStream(path);
-			try
-			{
-				save = new ObjectInputStream(saveFile);
-				mastermind = (Mastermind) save.readObject();
-				save.close();
-			} catch (StreamCorruptedException streamException) 
-			{    
-		        return null;
-		    }
-			
-		}
-		catch(Exception exc)
-		{
-			exc.printStackTrace();
-		}
-		
+		ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+		Mastermind mastermind = (Mastermind)objectInputStream.readObject();
+		objectInputStream.close();
+
 		return mastermind;
 	}	
 }
